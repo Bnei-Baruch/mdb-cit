03f804fed492a567804ea6d91e7ebe1a39ed7ec7
Edo Shor <edoshor@gmail.com>
2017-04-03 11:55:58 +0300

    Deployment script + example changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,9 @@ npm run build
 
 scp -r build/* archive@app.mdb.bbdomain.org:/sites/_tools/cit
 scp example.html archive@app.mdb.bbdomain.org:/sites/_tools/cit
+
+MAIN_JS="$(grep "\"main.js\":" build/asset-manifest.json | awk -F: '{print substr($2,3,length($2)-4)}')"
+MAIN_CSS="$(grep "\"main.css\":" build/asset-manifest.json | awk -F: '{print substr($2,3,length($2)-4)}')"
+
+ssh archive@app.mdb.bbdomain.org "ln -sf /sites/_tools/cit/$MAIN_JS /sites/_tools/cit/mdb-cit.js"
+ssh archive@app.mdb.bbdomain.org "ln -sf /sites/_tools/cit/${MAIN_CSS} /sites/_tools/cit/mdb-cit.css"
